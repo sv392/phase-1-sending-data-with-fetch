76d8816d2c08098b44c3bba01b97be2a3a8c2555
Sanh Vu <sanhvu020@gmail.com>
2022-11-15 21:23:23 -0500

    commit tree 29219eb1e539434e1b50a9002dd19f2463fba088

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+const userUrl = "http://localhost:3000/users"
+
+let bodyData = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+let objStuff = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body:JSON.stringify(bodyData)
+}
+
+function submitData() {
+    return fetch(userUrl, objStuff)
+        .then(response => response.json())
+        .then(getID)
+        .catch(errorFunc)
+}
+
+const bodyTag = document.querySelector('body')
+
+function getID(object) {
+    document.body.innerHTML = object.id
+    const li = document.createElement('li')
+    li.innerHTML = object.id
+    bodyTag.appendChild(li)
+}
+
+function errorFunc(error) {
+    document.body.innerHTML = error.message
+    const newLi = document.createElement('li')
+    newLi.innerHTML = error.message
+    bodyTag.appendChild(newLi)
+}
